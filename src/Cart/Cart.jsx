58356067d384496e00a921f24a394aee787d387a
Ayush Mazumdar <ayush.mazumdar111@gmail.com>
2024-08-31 10:14:27 +0530

    Added message for empty cart

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -6,24 +6,28 @@ function Cart(props){
     let totPrice=0;
     orderedMenu.map(food => totPrice=totPrice+(food.cartted*food.price));
 
-    return(
-    <>{orderedMenu.map((food,index) => 
-        <React.Fragment key={index}>
-            {food.cartted !=0 && 
-            
-            <div className={styles.Cart}>
-                <div className={styles.Item}>
-                    <p><b>Name : </b>{food.name}</p>
-                    <p><b>Items : </b>{food.cartted}</p>
-                    <p><b>Price : </b>{food.cartted*food.price}</p> 
-                </div>
-            </div>}
+    if(orderedMenu[0].cartted==0 && orderedMenu[1].cartted==0 && orderedMenu[2].cartted==0 && orderedMenu[3].cartted==0 && orderedMenu[4].cartted==0 && orderedMenu[5].cartted==0 && orderedMenu[6].cartted==0){
+        return(<h1 style={{textAlign:"center"}}>🛒 Your cart is empty 🙁</h1>)
+    }
+    else{
+        return(
+        <>{orderedMenu.map((food,index) => 
+            <React.Fragment key={index}>
+                {food.cartted !=0 && 
+                
+                <div className={styles.Cart}>
+                    <div className={styles.Item}>
+                        <p><b>Name : </b>{food.name}</p>
+                        <p><b>Items : </b>{food.cartted}</p>
+                        <p><b>Price : </b>{food.cartted*food.price}</p> 
+                    </div>
+                </div>}
 
-        </React.Fragment>
-        )}
-        <button className={styles.Pay}>Pay: ₹ {totPrice}</button>
-    </>)
-
+            </React.Fragment>
+            )}
+            <button className={styles.Pay}>Pay: ₹ {totPrice}</button>
+        </>)
+    }
 }
 
 export default Cart
